cb51cf588b9884e277f9cb2776ff041370274565
artkoro94 <artkoro94s@gmail.com>
2026-05-04 22:17:33 +0400

    feat: add pokemon card component

---
+++ src/components/pokemon-card.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import type { PokemonCardData } from '../shared/api/pokemon-api';
+
+interface PokemonCardProps {
+  pokemon: PokemonCardData;
+}
+
+export class PokemonCard extends React.Component<PokemonCardProps> {
+  render() {
+    const { pokemon } = this.props;
+
+    return (
+      <article className="pokemon-card">
+        <h3>{pokemon.name}</h3>
+        <p>{pokemon.description}</p>
+      </article>
+    );
+  }
+}
