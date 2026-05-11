724cd2ed19ae11c95c91d6af388ba95edc3cad78
artkoro94 <artkoro94s@gmail.com>
2026-05-11 20:25:47 +0400

    test: add search component  unit tests

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ErrorBoundary } from './components/error-boundary';
 import { ErrorButton } from './components/error-button';
 import { Header } from './components/header';
 import { Results } from './components/results/results';
-import { Search } from './components/search';
+import { Search } from './components/search/search';
 import { fetchPokemons, type PokemonCardData } from './shared/api/pokemon-api';
 
 interface AppState {
