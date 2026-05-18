d2b4334ce490c75c1b651445f600705349350fdb
artkoro94 <artkoro94s@gmail.com>
2026-05-18 23:23:46 +0400

    refactor: imporve styles

--- src/shared/test/pokemon-mocks.ts
+++ src/shared/test/pokemon-mocks.ts
@@ -4,12 +4,14 @@ export const pikachuMock: PokemonCardData = {
   id: 25,
   name: 'Pikachu',
   description: 'Types: electric. Height: 4. Weight: 60.',
+  image: ''
 };
 
 export const bulbasaurMock: PokemonCardData = {
   id: 1,
   name: 'Bulbasaur',
   description: 'Types: grass, poison. Height: 7. Weight: 69.',
+  image: ''
 };
 
 export const pokemonListMock: PokemonCardData[] = [
