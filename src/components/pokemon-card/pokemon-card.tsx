dfa335809f7b19eb8588d0252cf3403fd83a769d
artkoro94 <artkoro94s@gmail.com>
2026-05-18 22:28:23 +0400

    refactor: pokemon card component

--- src/components/pokemon-card/pokemon-card.tsx
+++ src/components/pokemon-card/pokemon-card.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import { Link, useSearchParams } from 'react-router';
 import type { PokemonCardData } from '../../shared/api/pokemon-api';
 
 interface PokemonCardProps {
   pokemon: PokemonCardData;
 }
 
-export class PokemonCard extends React.Component<PokemonCardProps> {
-  render() {
-    const { pokemon } = this.props;
+export const PokemonCard = ({ pokemon }: PokemonCardProps) => {
+  const [searchParams] = useSearchParams();
+  const nextSearchParams = new URLSearchParams(searchParams);
 
-    return (
-      <article className="pokemon-card">
+  if (!nextSearchParams.get('page')) {
+    nextSearchParams.set('page', '1');
+  }
+
+  return (
+    <Link
+      className="pokemon-card"
+      to={`/pokemon/${pokemon.id}?${nextSearchParams.toString()}`}
+    >
+      <article>
         <h3>{pokemon.name}</h3>
         <p>{pokemon.description}</p>
       </article>
-    );
-  }
-}
+    </Link>
+  );
+};
