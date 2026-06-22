726f6963231aca8193937981d0913d4c63263869
artkoro94 <artkoro94s@gmail.com>
2026-06-22 20:46:25 +0400

    feat: add 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,14 @@
 import Link from 'next/link';
 
-export default function AboutPage() {
+export default function NotFound() {
   return (
     <main className="app">
       <section className="page-card">
-        <h1>About</h1>
+        <h1>404</h1>
 
-        <p>
-          This app was created by @artkoro94 as part of the RS School React
-          course.
-        </p>
+        <p>Page was not found.</p>
 
-        <a
-          className="button"
-          href="https://rs.school/courses/reactjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          RS School React Course
-        </a>
-
-        <Link className="header__link" href="/">
+        <Link className="button" href="/">
           Back to app
         </Link>
       </section>
