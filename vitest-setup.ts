520761f1d7b0141c9c0848bcf9eb364beb2d0f6d
artkoro94 <artkoro94s@gmail.com>
2026-06-23 02:44:51 +0400

    fix: vitest setup fo tests

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,1 +1,22 @@
 import '@testing-library/jest-dom/vitest';
+import { vi } from 'vitest';
+import React from 'react';
+
+vi.mock('./src/i18n/navigation', () => ({
+  Link: ({
+    children,
+    href,
+    ...props
+  }: {
+    children: React.ReactNode;
+    href: string;
+  }) =>
+    React.createElement(
+      'a',
+      {
+        href,
+        ...props,
+      },
+      children
+    ),
+}));
